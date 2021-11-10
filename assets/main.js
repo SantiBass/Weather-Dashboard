c1cd3f0a41cc19344f3d78649854010398153547
SantiBass <sibarraryan@gmail.com>
2021-11-10 12:52:16 -0800

    add-wheathe-icons-working

--- assets/main.js
+++ assets/main.js
@@ -5,8 +5,14 @@ var storeItem = localStorage.getItem("storeItem");
 console.log(cityName);
 //    console.log(fetchButtonEl);
 console.log(storeItem);
-var currentHour = (moment().format('k'));
+// var currentHour = (moment().format('k'));
 // });
+
+setInterval(() => {
+    var time = moment().format('MMMM Do YYYY, h:mm:ss a'); //present time
+    $("#courrentHour").text(time)
+
+}, 1000);
 
 
 
@@ -27,12 +33,14 @@ function getTextValue() {
     document.getElementById("searchArea").innerHTML = storeItem + " " + input;
 }
 
-
+var weather = {
+    "key": 'ffb1f81a0f40a4da0ddb506ba11d89ce'
+};
 
 
 function getApi(arg) {
 console.log(arg)
-    
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + arg + '&appid=ffb1f81a0f40a4da0ddb506ba11d89ce'
     // var icon = " http://openweathermap.org/img/wn/10@2x.png";
     //  $(".weatherIcon").text(data.list[0].weather.weatherIcon);//date and time
     fetch(requestUrl)
@@ -43,48 +51,50 @@ console.log(arg)
             //Using console.log to examine the data
             console.log(data);
 
-            
-            $("#weatherIcon").text(data.list[0].weather[0].icon);
+            $("#fiveDaysForecast").text("Five Days Forecast");
+            $("#weatherIcon").scr =" http://openweathermap.org/img/wn/" +(data.list[0].weather[0].icon) + "@2x.png";
+            $("#weatherIcon0").text(data.list[0].weather[0].icon);
             $("#time").text("Date: " + data.list[0].dt_txt);//date and time
-            $("#cityName").text("The Weather in:  "  + data.city.name); // name of the city
+            $("#cityName").text("The Weather in "+ data.city.name + " is:"); // name of the city
             $("#tempName").text("Temperature: " + data.list[0].main.temp +" degrees.");// temperature
             $("#humidityName").text("Humidity: " + data.list[0].main.humidity);// humidity
             $("#windSpeed").text("Wind Speed: " + data.list[0].wind.speed); // wind
             
-            $("#weatherIcon").text(data.list[1].weather[0].icon);
-            $("#time1").text("Date: " + data.list[1].dt_txt);//date and time
-            $("#cityName1").text("The Weather in:  "  + data.city.name); // name of the city
+            $("#weatherIcon1").text(data.list[0].weather[0].icon);
+            
+            $("#time1").text("Date: " + data.list[0].dt_txt);//date and time
+            // $("#cityName1").text("The Weather in "  + data.city.name + " is:"); // name of the city
             $("#tempName1").text("Temperature: " + data.list[1].main.temp);// temperature
             $("#humidityName1").text("Humidity: " + data.list[1].main.humidity);// humidity
             $("#windSpeed1").text("Wind Speed: " + data.list[1].wind.speed); // wind
             // $(".weatherCard").text()
-            $("#weatherIcon").text(data.list[2].weather[0].icon);
-            $("#time2").text("Date: " + data.list[2].dt_txt);//date and time
-            $("#cityName2").text("The Weather in:  "  + data.city.name); // name of the city
+            $("#weatherIcon2").text(data.list[8].weather[0].icon);
+            $("#time2").text("Date: " + data.list[8].dt_txt);//date and time
+            // $("#cityName2").text("The Weather in:  "  + data.city.name); // name of the city
             $("#tempName2").text("Temperature: " + data.list[2].main.temp);// temperature
             $("#humidityName2").text("Humidity: " + data.list[2].main.humidity);// humidity
             $("#windSpeed2").text("Wind Speed: " + data.list[2].wind.speed); // wind
             // $(".weatherCard2").text()
             
-            $("#weatherIcon").text(data.list[3].weather[0].icon);
-            $("#time3").text("Date: " + data.list[3].dt_txt);//date and time
-            $("#cityName3").text("The Weather in:  "  + data.city.name); // name of the city
+            $("#weatherIcon3").text(data.list[16].weather[0].icon);
+            $("#time3").text("Date: " + data.list[16].dt_txt);//date and time
+            // $("#cityName3").text("The Weather in:  "  + data.city.name); // name of the city
             $("#tempName3").text("Temperature: " + data.list[3].main.temp);// temperature
             $("#humidityName3").text("Humidity: " + data.list[3].main.humidity);// humidity
             $("#windSpeed3").text("Wind Speed: " + data.list[3].wind.speed); // wind
             // $(".weatherCard").text()
             
-            $("#weatherIcon").text(data.list[4].weather[0].icon);
-            $("#time4").text("Date: " + data.list[4].dt_txt);//date and time
-            $("#cityName4").text("The Weather in:  "  + data.city.name); // name of the city
+            $("#weatherIcon4").text(data.list[24].weather[0].icon);
+            $("#time4").text("Date: " + data.list[24].dt_txt);//date and time
+            // $("#cityName4").text("The Weather in:  "  + data.city.name); // name of the city
             $("#tempName4").text("Temperature: " + data.list[4].main.temp);// temperature
             $("#humidityName4").text("Humidity: " + data.list[4].main.humidity);// humidity
             $("#windSpeed4").text("Wind Speed: " + data.list[4].wind.speed); // wind
             // $(".weatherCard").text()
             
-            $("#weatherIcon").text(data.list[5].weather[0].icon);
-            $("#time5").text("Date: " + data.list[5].dt_txt);//date and time
-            $("#cityName5").text("The Weather in:  "  + data.city.name); // name of the city
+            $("#weatherIcon5").text(data.list[32].weather[0].icon);
+            $("#time5").text("Date: " + data.list[32].dt_txt);//date and time
+            // $("#cityName5").text("The Weather in:  "  + data.city.name); // name of the city
             $("#tempName5").text("Temperature: " + data.list[5].main.temp);// temperature
             $("#humidityName5").text("Humidity: " + data.list[5].main.humidity);// humidity
             $("#windSpeed5").text("Wind Speed: " + data.list[5].wind.speed); // wind
